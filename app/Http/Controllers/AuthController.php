3cb4e2c8dec7e297dd73fe96dd235203d1b3156e
hamed gh <hamedgh3285@gmail.com>
2023-12-07 22:44:23 +0330

    auth login function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
@@ -18,7 +20,17 @@ class AuthController extends Controller
         $token = $user->createToken('register_token')->plainTextToken;
         return response([
             'user' => $user ,
-            'api_token' => $token
+            'token' => $token
         ]);
+    }
+    public function login(LoginRequest $request)
+    {
+        if (!auth()->attempt(['email'=>$request->email , 'password'=>$request->password])) {
+            return response(['message' => 'Invalid credentials']);
+        }
+        else {
+            $user = Auth::user();
+            $api_token = $user->createToken('login_token')->plainTextToken;
+            return response(['user' => auth()->user(), 'token' => $api_token]);}
     }
 }
