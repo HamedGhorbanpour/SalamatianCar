77799a96e88ad9c067faddff55a56415d64bf5d3
hamed gh <hamedgh3285@gmail.com>
2023-12-12 02:16:46 +0330

    create user requests

--- app/Http/Requests/User/CreateUserRequest.php
+++ app/Http/Requests/User/CreateUserRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\Auth;
+namespace App\Http\Requests\User;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class RegisterRequest extends FormRequest
+class CreateUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,7 +24,7 @@ class RegisterRequest extends FormRequest
         return [
             'name' => 'required|max:55',
             'email' => 'email|required|unique:users',
-            'password' => 'required|string|min:3',
+            'password' => 'required|string|min:8',
         ];
     }
 }
