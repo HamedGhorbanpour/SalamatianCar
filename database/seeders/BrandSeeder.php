da9d246f432fa28d7f04e1957b05fbad1b0f7b09
hamed gh <hamedgh3285@gmail.com>
2023-12-25 19:31:27 +0330

    add brand seeder

---
+++ database/seeders/BrandSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Brand;
+
+class BrandSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Brand::create([
+            'name' => 'MVM' ,
+            'user_id' => 1 ,
+        ],[
+            'name' => 'Fownix' ,
+            'user_id' => 1 ,
+        ]);
+
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(PercentSeeder::class);
+        $this->call(BrandSeeder::class);
     }
 }
