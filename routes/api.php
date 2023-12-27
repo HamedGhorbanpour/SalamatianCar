a1526b3babd5053a504fb18c03a97b85293609c4
hamed gh <hamedgh3285@gmail.com>
2023-12-27 22:44:55 +0330

    finish the route changes

--- routes/api.php
+++ routes/api.php
@@ -13,49 +13,53 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+// Non-authenticated routes
+Route::namespace('App\Http\Controllers\Panel')->group(function () {
+    // Cars
+    Route::prefix('/cars')->controller('CarController')->group(function () {
+        Route::get('/', 'index');
+    });
+    // Brands
+    Route::prefix('/brands')->controller('BrandController')->group(function () {
+        Route::get('/', 'index');
+    });
+    // Percents
+    Route::prefix('/percents')->controller('PercentController')->group(function () {
+        Route::get('/', 'index');
+    });
+});
 
-// Panel
+// Authenticated routes
 Route::namespace('App\Http\Controllers\Panel')
     ->middleware('auth:sanctum')
     ->group(function() {
-    // Cars
-    Route::prefix('/cars')->controller('CarController')->group(function (){
-        Route::post('/','store',);
-        Route::get('/{car}','show');
-        Route::patch('/{car}','update');
-        Route::delete('/{car}','destroy');
+        // Cars
+        Route::prefix('/cars')->controller('CarController')->group(function (){
+            Route::post('/', 'store');
+            Route::get('/{car}', 'show');
+            Route::patch('/{car}', 'update');
+            Route::delete('/{car}', 'destroy');
+        });
+        // Brands
+        Route::prefix('/brands')->controller('BrandController')->group(function (){
+            Route::post('/', 'store');
+            Route::get('/{brand}', 'show');
+            Route::patch('/{brand}', 'update');
+            Route::delete('/{brand}', 'destroy');
+        });
+        // Users
+        Route::prefix('/users')->controller('UserController')->group(function (){
+            Route::get('/', 'index');
+            Route::post('/', 'store');
+            Route::get('/{user}', 'show');
+            Route::patch('/{user}', 'update');
+            Route::delete('/{user}', 'destroy');
+        });
+        // Taxes & Benefits
+        Route::prefix('/percents')->controller('PercentController')->group(function (){
+            Route::patch('/', 'update');
+        });
     });
-    // Brands
-    Route::prefix('/brands')->controller('BrandController')->group(function (){
-        Route::post('/','store',);
-        Route::get('/{brand}','show');
-        Route::patch('/{brand}','update');
-        Route::delete('/{brand}','destroy');
-    });
-    // Users
-    Route::prefix('/users')->controller('UserController')->group(function (){
-        Route::get('/','index');
-        Route::post('/','store');
-        Route::get('/{user}','show');
-        Route::patch('/{user}','update');
-        Route::delete('/{user}','destroy');
-    });
-    // Taxes & Benefits
-    Route::prefix('/percents')->controller('PercentController')->group(function (){
-        Route::patch('/','update');
-    });
-});
-Route::namespace('App/Http/Controllers/Panel')->group(function (){
-    Route::prefix('/cars')->controller('CarController')->group(function (){
-        Route::get('/','index',);
-    });
-    Route::prefix('/brands')->controller('BrandController')->group(function (){
-        Route::post('/','index',);
-    });
-    Route::prefix('/percents')->controller('BrandController')->group(function (){
-        Route::get('/','index',);
-    });
-});
 // Auth
 Route::namespace('App\Http\Controllers')->group(function ($router) {
     Route::prefix('/auth')->controller('AuthController')->group(function (){
